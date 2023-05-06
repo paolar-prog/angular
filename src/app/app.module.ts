8bcca314bb46e23bafa452097bb3d9c72a468c20
YENNY PAOLA RAMOS CARRILLO <yramosca@contratista.epm.co>
2023-05-06 09:25:17 -0500

    Primer ejercicio en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
